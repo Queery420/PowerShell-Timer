40169b39e3dad6386c909006a90b0628fbf4e4ea
Queeryeverything <46384594+Queeryeverything@users.noreply.github.com>
2021-06-02 15:38:17 -0400

    Update Timer.ps1
    v0.2 - formatted text, added notification support via BurntToast (required to be installed, see ReadMe)

--- Timer.ps1
+++ Timer.ps1
@@ -3,6 +3,9 @@
 # Displays current time, end time, remaining time.
 # Plays a sound and pushes a notification when the time is up.
 
+# Windows 10 Notification support
+Import-Module BurntToast
+
 # startTime is the time the script is initialized
 $startTime = $(get-date)
 $startTimeStr = $startTime.ToShortTimeString()
@@ -14,15 +17,38 @@ $minutes = 30
 $endTime = $(get-date).AddMinutes($minutes)
 $endTimeStr = $endTime.ToShortTimeString()
 
+# Display start & end time
+clear-host
+write-host "     Start:   $startTimeStr"
+write-host "     End:     $endTimeStr"
+write-host "     $minutes minutes remaining."
+
+# wait 1 minute
+Start-Sleep -s 60
+
+# Update currentTime and check to see if the timer is finished.
+$currentTime = $(get-date)
+$currentTimeStr = $currentTime.ToShortTimeString()
+$minutes = $minutes - 1
+
+# Core Loop. Updates every 60 seconds,
 While ($currentTime -le $endTime)
 {
-  # Displays current time, makes a joke, tells you the rest of it idk
+  # Displays start time, end time, current time, remaining minutes
   clear-host
-  write-host "The time is now $currentTimeStr"
-  write-host "This timer was started at $startTimeStr"
-  write-host "The timer will ring at $endTimeStr, which will signal the endTime."
-  write-host "You have $minutes minutes remaining to make your peace."
+  write-host "     Start:   $startTimeStr"
+  write-host "     End:     $endTimeStr"
+  write-host "     Now:     $currentTimeStr"
+  if($minutes -eq 1)
+  {
+    write-host "     1 minute remaining."
+  }
+  else
+  {
+    write-host "     $minutes minutes remaining."
+  }
 
+  # wait 1 minute
   Start-Sleep -s 60
 
   # Update currentTime and check to see if the timer is finished.
@@ -31,5 +57,7 @@ While ($currentTime -le $endTime)
   $minutes = $minutes - 1
 }
 
+# Final time announcement, push notification
 clear-host
-write-host "The time is now $currentTimeStr. You have lived to see it through once more."
+write-host "The time is now $currentTimeStr."
+New-BurntToastNotification -Text "PowerShell Timer","The time is now $currentTimeStr."
